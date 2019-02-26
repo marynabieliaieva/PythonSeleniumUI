5bd46855f999c0cdaa568b0a26e5eb95648dd155
marynelko <marynelko@gmail.com>
2019-02-26 16:59:50 +0100

    framework refactoring

--- pages/home/login_page.py
+++ pages/home/login_page.py
@@ -1,20 +1,44 @@
 from selenium.webdriver.common.by import By
+from base.selenium_driver import SeleniumDriver
 
-class LoginPage():
+class LoginPage(SeleniumDriver):
 
     def __init__(self, driver):
+        super().__init__(driver)
         self.driver = driver
 
-    def login(self, username, password):
-        loginLink = self.driver.find_element(By.LINK_TEXT, "Login")
-        loginLink.click()
+    # Locators
+    _login_link = "Login"
+    _email_field = "user_email"
+    _password_field = "user_password"
+    _login_button = "commit"
 
-        emailField = self.driver.find_element(By.ID, "user_email")
-        emailField.send_keys(username)
+    # def getLoginLink(self):
+    #     return self.driver.find_element(By.LINK_TEXT, self._login_link)
+    #
+    # def getEmailField(self):
+    #     return self.driver.find_element(By.ID, self._email_field)
+    #
+    # def getPasswordField(self):
+    #     return self.driver.find_element(By.ID, self._password_field)
+    #
+    # def getLoginButton(self):
+    #     return self.driver.find_element(By.NAME, self._login_button)
 
-        passwordField = self.driver.find_element(By.ID, "user_password")
-        passwordField.send_keys(password)
+    def clickLoginLink(self):
+        self.elementClick(self._login_link, locatorType="link")
 
-        loginButton = self.driver.find_element(By.NAME, "commit")
-        loginButton.click()
+    def enterEmail(self, email):
+        self.sendKeys(email, self._email_field)
 
+    def enterPassword(self, password):
+        self.sendKeys(password, self._password_field)
+
+    def clickLoginButton(self):
+        self.elementClick(self._login_button, locatorType="name")
+
+    def login(self, email, password):
+        self.clickLoginLink()
+        self.enterEmail(email)
+        self.enterPassword(password)
+        self.clickLoginButton()
